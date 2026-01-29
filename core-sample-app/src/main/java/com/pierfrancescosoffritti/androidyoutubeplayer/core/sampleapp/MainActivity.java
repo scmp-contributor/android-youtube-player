9a80926749092303efee8d0d6337f59f01ca11d9
Woody Lam <woody.lam@scmp.com>
2026-01-29 11:24:46 +0000

    REMOVED: com.psoffritti.librarysampleapptemplate

--- core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/MainActivity.java
+++ core-sample-app/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/sampleapp/MainActivity.java
@@ -4,23 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.SCMPExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.completeExample.CompleteExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.chromecastExample.ChromeCastExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.customUiExample.CustomUiActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.fragmentExample.FragmentExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.iFramePlayerOptionsExample.IFramePlayerOptionsExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.liveVideoExample.LiveVideoActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.noLifecycleObserverExample.NoLifecycleObserverExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.pictureInPictureExample.PictureInPictureActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.playerStateExample.PlayerStateActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.recyclerViewExample.RecyclerViewActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.simpleExample.SimpleExampleActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.viewPagerExample.ViewPagerActivity;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.sampleapp.examples.nativeUiExample.NativeUiExampleActivity;
-import com.pierfrancescosoffritti.aytplayersample.R;
-import com.psoffritti.librarysampleapptemplate.core.Constants;
-import com.psoffritti.librarysampleapptemplate.core.SampleAppTemplateActivity;
-import com.psoffritti.librarysampleapptemplate.core.utils.ExampleActivityDetails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,32 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, SampleAppTemplateActivity.class);
-
-        intent.putExtra(Constants.TITLE.name(), getString(R.string.app_name));
-        intent.putExtra(Constants.GITHUB_URL.name(), "https://github.com/PierfrancescoSoffritti/android-youtube-player/");
-        intent.putExtra(Constants.HOMEPAGE_URL.name(), "https://pierfrancescosoffritti.github.io/android-youtube-player/");
-
-        ExampleActivityDetails[] examples = new ExampleActivityDetails[]{
-                new ExampleActivityDetails(R.string.scmp_example, null, SCMPExampleActivity.class),
-                new ExampleActivityDetails(R.string.simple_example, null, SimpleExampleActivity.class),
-                new ExampleActivityDetails(R.string.complete_example, null, CompleteExampleActivity.class),
-                new ExampleActivityDetails(R.string.native_ui_example, null, NativeUiExampleActivity.class),
-                new ExampleActivityDetails(R.string.custom_ui_example, null, CustomUiActivity.class),
-                new ExampleActivityDetails(R.string.recycler_view_example, null, RecyclerViewActivity.class),
-                new ExampleActivityDetails(R.string.view_pager_example, null, ViewPagerActivity.class),
-                new ExampleActivityDetails(R.string.fragment_example, null, FragmentExampleActivity.class),
-                new ExampleActivityDetails(R.string.live_video_example, null, LiveVideoActivity.class),
-                new ExampleActivityDetails(R.string.player_status_example, null, PlayerStateActivity.class),
-                new ExampleActivityDetails(R.string.picture_in_picture_example, null, PictureInPictureActivity.class),
-                new ExampleActivityDetails(R.string.chromecast_example, null, ChromeCastExampleActivity.class),
-                new ExampleActivityDetails(R.string.iframe_player_options_example, null, IFramePlayerOptionsExampleActivity.class),
-                new ExampleActivityDetails(R.string.no_lifecycle_observer_example, null, NoLifecycleObserverExampleActivity.class)
-        };
-
-        intent.putExtra(Constants.EXAMPLES.name(), examples);
-
+        Intent intent = new Intent(this, SCMPExampleActivity.class);
         startActivity(intent);
         finish();
     }
